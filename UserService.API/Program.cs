c93e850b59cc8197dd74b6e54facfed3f4b4d65d
Illya <illamatviuk@gmail.com>
2025-06-14 16:55:09 +0300

    Added program settings.

--- UserService.API/Program.cs
+++ UserService.API/Program.cs
@@ -1,24 +1,31 @@
+using UserService.Application.Commands.CreateUser;
+using UserService.Application.Validators;
+using FluentValidation;
+using UserService.Application.Mappings;
+using UserService.Infrastructure.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("PostgreSQLConnection")!);
+
+builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(RegisterUserCommandHandler).Assembly));
+
+builder.Services.AddValidatorsFromAssembly(typeof(CreateUserValidator).Assembly);
+builder.Services.AddAutoMapper(typeof(UserMappingProfile).Assembly);
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
