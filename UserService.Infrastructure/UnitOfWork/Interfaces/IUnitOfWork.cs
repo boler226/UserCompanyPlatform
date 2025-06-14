2a27f48c3282fa5b2125be649ee12c68a4cd7f14
Illya <illamatviuk@gmail.com>
2025-06-14 16:54:31 +0300

    Added UnitOfWork.

---
+++ UserService.Infrastructure/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Infrastructure.UnitOfWork.Interfaces {
+    public interface IUnitOfWork {
+        IUserRepository Users { get; }
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ UserService.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using UserService.Domain.Interfaces;
+using UserService.Infrastructure.DbContext;
+using UserService.Infrastructure.UnitOfWork.Interfaces;
+
+namespace UserService.Infrastructure.UnitOfWork {
+    public class UnitOfWork(
+        UserDbContext context,
+        IUserRepository userRepository
+        ) : IUnitOfWork {
+        public IUserRepository Users => userRepository;
+
+        public async Task<int> SaveChangesAsync() =>
+            await context.SaveChangesAsync();
+    }
+}
