d3e5807a4847ebcb611f139159c3bb5924733d58
Illya <illamatviuk@gmail.com>
2025-06-14 16:44:32 +0300

    Added CreateUser validator.

---
+++ UserService.Application/Validators/CreateUserValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Application.Commands.CreateUser;
+
+namespace UserService.Application.Validators {
+    public class CreateUserValidator : AbstractValidator<RegisterUserCommand> {
+        public CreateUserValidator() {
+            RuleFor(u => u.FirstName).NotEmpty().MaximumLength(100);
+            RuleFor(u => u.LastName).NotEmpty().MaximumLength(100);
+            RuleFor(u => u.Email).NotEmpty().EmailAddress();
+            RuleFor(u => u.Password).NotEmpty().MinimumLength(6);
+        }
+    }
+}
