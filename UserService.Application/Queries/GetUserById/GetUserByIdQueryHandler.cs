4dff0ebc6539ce39b320e1f047fff3c0745d2339
Illya <illamatviuk@gmail.com>
2025-06-14 16:45:02 +0300

    Added LoginUser query.

---
+++ UserService.Application/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using MediatR;
+using UserService.Application.DTOs;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Application.Queries.GetUserById
+{
+    public class GetUserByIdQueryHandler(
+        IUserRepository userRepository,
+        IMapper mapper
+        ) : IRequestHandler<GetUserByIdQuery, UserDto>
+    {
+        public async Task<UserDto> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            var user = await userRepository.GetByIdAsync(request.id);
+
+            if (user is null)
+                throw new Exception("User not found");
+
+            return mapper.Map<UserDto>(user);
+        }
+    }
+}
