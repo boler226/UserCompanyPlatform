be35e45f516f4a078f00879b045196c7cc8c1829
Illya <illamatviuk@gmail.com>
2025-06-14 16:43:35 +0300

    Added mapping profile.

---
+++ UserService.Application/Mappings/UserMappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using UserService.Application.DTOs;
+using UserService.Domain.Entities;
+
+namespace UserService.Application.Mappings {
+    public class UserMappingProfile : Profile {
+        public UserMappingProfile() {
+            CreateMap<User, UserDto>();
+        }
+    }
+}
