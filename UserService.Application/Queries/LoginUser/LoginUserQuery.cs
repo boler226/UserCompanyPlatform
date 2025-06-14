f1098c5d8d6070b6042217aa97df56d588a583ee
Illya <illamatviuk@gmail.com>
2025-06-14 16:53:13 +0300

    Added LoginUser query.

---
+++ UserService.Application/Queries/LoginUser/LoginUserQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Application.Queries.LoginUser {
+    public record LoginUserQuery(string Email, string Password) : IRequest<string>;
+}
---
+++ UserService.Application/Queries/LoginUser/LoginUserQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.Configuration;
+using UserService.Domain.Interfaces;
+using UserService.Infrastructure.UnitOfWork.Interfaces;
+
+namespace UserService.Application.Queries.LoginUser {
+    public class LoginUserQueryHandler(
+        IUnitOfWork unitOfWork,
+        IJwtTokenService jwtTokenService
+        ) : IRequestHandler<LoginUserQuery, string> {
+        public async Task<string> Handle(LoginUserQuery request, CancellationToken cancellationToken) {
+            var user = await unitOfWork.Users.GetByEmailAsync(request.Email)
+                    ?? throw new Exception("Invalid credentials");
+            if (user is null || !BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+                throw new Exception("Invalid credentials");
+
+           return await jwtTokenService.GenerateTokenAsync(user);
+        }
+    }
+}
