82f634cece5bb5b1a9612091f24a7c47646762a2
Illya <illamatviuk@gmail.com>
2025-06-14 16:39:17 +0300

    Added User and Role entites.

---
+++ UserService.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using UserService.Domain.Enums;
+
+namespace UserService.Domain.Entities {
+    public class User : IdentityUser<Guid> {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public DateTime RegisteredAt { get; set; } = DateTime.UtcNow;
+        public Role Role { get; set; } = Role.User;
+        public Guid? CompanyId { get; set; }
+    }
+}
