69f36220dfa3893b71a8282cd2ce457494daa442
Illya <illamatviuk@gmail.com>
2025-06-14 16:52:55 +0300

    Added GetUserById query.

---
+++ UserService.Application/Queries/GetUserById/GetUserByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using UserService.Application.DTOs;
+
+namespace UserService.Application.Queries.GetUserById {
+    public record GetUserByIdQuery(Guid id) : IRequest<UserDto>;
+}
---
+++ UserService.Application/Queries/GetUserById/GetUserByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Application.DTOs;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Application.Queries.GetUserById {
+    public class GetUserByIdQueryHandler(
+       IUserRepository userRepository,
+       IMapper mapper
+       ) : IRequestHandler<GetUserByIdQuery, UserDto> {
+        public async Task<UserDto> Handle(GetUserByIdQuery request, CancellationToken cancellationToken) {
+            var user = await userRepository.GetByIdAsync(request.id);
+
+            if (user is null)
+                throw new Exception("User not found");
+
+            return mapper.Map<UserDto>(user);
+        }
+    }
+}
