3059a3d3e6a2c919b7719196c0e7cb965b405cdf
Illya <illamatviuk@gmail.com>
2025-06-14 16:44:16 +0300

    Added RegisterUser command.

---
+++ UserService.Application/Commands/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace UserService.Application.Commands.CreateUser
+{
+    public record RegisterUserCommand(string FirstName, string LastName, string Email, string Password) : IRequest<Guid>;
+}
---
+++ UserService.Application/Commands/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using MediatR;
+using UserService.Domain.Entities;
+using UserService.Infrastructure.UnitOfWork.Interfaces;
+
+
+namespace UserService.Application.Commands.CreateUser {
+    public class RegisterUserCommandHandler(
+        IUnitOfWork unitOfWork,
+        IValidator<RegisterUserCommand> validator
+        ) : IRequestHandler<RegisterUserCommand, Guid> {
+        public async Task<Guid> Handle(RegisterUserCommand request, CancellationToken cancellationToken) {
+            await validator.ValidateAndThrowAsync(request, cancellationToken);
+
+            var user = new User {
+                Id = Guid.NewGuid(),
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(request.Password),
+                RegisteredAt = DateTime.UtcNow
+            };
+
+            await unitOfWork.Users.AddAsync(user);
+            await unitOfWork.SaveChangesAsync();
+
+            return user.Id;
+        }
+    }
+}
