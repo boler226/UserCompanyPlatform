82d943abf57d7dd97507c840d2deacc43427dd53
Illya <illamatviuk@gmail.com>
2025-06-14 16:44:44 +0300

    Added GetAllUser query.

---
+++ UserService.Application/Queries/GetAllUsers/GetAllUsersQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Application.DTOs;
+
+namespace UserService.Application.Queries.GetAllUsers {
+    public record GetAllUsersQuery : IRequest<List<UserDto>>;
+}
---
+++ UserService.Application/Queries/GetAllUsers/GetAllUsersQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Application.DTOs;
+using UserService.Domain.Interfaces;
+
+namespace UserService.Application.Queries.GetAllUsers {
+    internal class GetAllUsersQueryHandler(
+        IUserRepository userRepository,
+        IMapper mapper
+        ) : IRequestHandler<GetAllUsersQuery, List<UserDto>> {
+        public async Task<List<UserDto>> Handle(GetAllUsersQuery requst, CancellationToken cancellationToken) {
+            var users = await userRepository.GetAllAsync();
+            return mapper.Map<List<UserDto>>(users);
+        }
+    }
+}
