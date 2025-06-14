f6358def72559aba23087a8782d80d6fc1ed3731
Illya <illamatviuk@gmail.com>
2025-06-14 16:41:10 +0300

    Added User database context and migrations.

---
+++ UserService.Infrastructure/DbContext/UserDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UserService.Domain.Entities;
+
+namespace UserService.Infrastructure.DbContext {
+    public class UserDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid> {
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) {}
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity => {
+                entity.HasKey(u => u.Id);
+                entity.Property(u => u.FirstName).IsRequired().HasMaxLength(100);
+                entity.Property(u => u.LastName).IsRequired().HasMaxLength(100);
+                entity.Property(u => u.Email).IsRequired();
+                entity.Property(u => u.PasswordHash).IsRequired();
+            });
+        }
+    }
+}
