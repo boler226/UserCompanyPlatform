ea451267bdd02a2e14e15be3138255f6ca1c91f9
Illya <illamatviuk@gmail.com>
2025-06-14 16:41:35 +0300

    Added User repository.

---
+++ UserService.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using UserService.Domain.Entities;
+
+namespace UserService.Domain.Interfaces {
+    public interface IUserRepository {
+        Task<List<User>> GetAllAsync();
+        Task<User?> GetByIdAsync(Guid id);
+        Task<User?> GetByEmailAsync(string email);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+    }
+}
---
+++ UserService.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Domain.Entities;
+using UserService.Domain.Interfaces;
+using UserService.Infrastructure.DbContext;
+
+namespace UserService.Infrastructure.Repositories {
+    public class UserRepository(UserDbContext context) : IUserRepository {
+        public async Task<List<User>> GetAllAsync() =>
+            await context.Users.ToListAsync();
+
+        public async Task<User?> GetByIdAsync(Guid id) =>
+            await context.Users.FirstOrDefaultAsync(u => u.Id == id);
+
+        public async Task<User?> GetByEmailAsync(string email) =>
+            await context.Users.FirstOrDefaultAsync(u => u.Email == email);
+
+        public async Task AddAsync(User user) {
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(User user) {
+            context.Users.Update(user);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(User user) {
+            context.Users.Remove(user);
+            await context.SaveChangesAsync();
+        }
+    }
+}
