71323c56dbcfabac5cb191159893c9a350d40c79
Illya <illamatviuk@gmail.com>
2025-06-14 16:43:02 +0300

    Added User and Login DTO's.

---
+++ UserService.Application/DTOs/UserDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Application.DTOs {
+    public class UserDto {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime RegisteredAt { get; set; }
+        public string Role { get; set; } = string.Empty;
+        public Guid? CompanyId { get; set; }
+    }
+}
