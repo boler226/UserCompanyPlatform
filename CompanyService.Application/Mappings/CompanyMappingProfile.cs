e39c4995b3287383d10c2d3b8dfd7b123c0a86c3
Illya <illamatviuk@gmail.com>
2025-06-25 21:18:11 +0300

    Added Company Mappings.

---
+++ CompanyService.Application/Mappings/CompanyMappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using CompanyService.Application.DTOs;
+using CompanyService.Domain.Entities;
+
+namespace CompanyService.Application.Mappings {
+    public class CompanyMappingProfile : Profile {
+        public CompanyMappingProfile() {
+            CreateMap<Company, CompanyDto>();
+        }
+    }
+}
