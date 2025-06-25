9e69aecf68bcc9a26bdc30e6e8aad4c8afeb1e52
Illya <illamatviuk@gmail.com>
2025-06-25 21:15:50 +0300

    Added CompanyRepository.

---
+++ CompanyService.Domain/Interfaces/ICompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using CompanyService.Domain.Entities;
+
+namespace CompanyService.Domain.Interfaces {
+    public interface ICompanyRepository {
+        Task<List<Company>> GetAllAsync(CancellationToken cancellationToken);
+        Task<Company?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<Company?> GetByEmailAsync(string email, CancellationToken cancellationToken);
+        Task AddAsync(Company user, CancellationToken cancellationToken);
+        Task UpdateAsync(Company user, CancellationToken cancellationToken);
+        Task DeleteAsync(Company user, CancellationToken cancellationToken);
+    }
+}
---
+++ CompanyService.Infrastructure/Repositories/CompanyRepository.cs
@@ -0,0 +1,34 @@
+﻿using CompanyService.Domain.Entities;
+using CompanyService.Domain.Interfaces;
+using CompanyService.Infrastructure.DbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyService.Infrastructure.Repositories {
+    public class CompanyRepository(
+        CompanyDbContext context
+        ) : ICompanyRepository {
+        public async Task<List<Company>> GetAllAsync (CancellationToken cancellationToken) =>
+            await context.Companies.ToListAsync(cancellationToken);
+
+        public async Task<Company?> GetByIdAsync(Guid id, CancellationToken cancellationToken) => 
+            await context.Companies.FirstOrDefaultAsync(c => c.Id == id, cancellationToken);
+
+        public async Task<Company?> GetByEmailAsync(string email, CancellationToken cancellationToken) =>
+            await context.Companies.FirstOrDefaultAsync(c => c.Email == email, cancellationToken);
+
+        public async Task AddAsync(Company company, CancellationToken cancellationToken) {
+            await context.Companies.AddAsync(company, cancellationToken);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateAsync(Company company, CancellationToken cancellationToken) {
+            context.Companies.Update(company);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(Company company, CancellationToken cancellationToken) {
+            context.Companies.Remove(company);
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
