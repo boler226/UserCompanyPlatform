176696d8a8b420b59ef4ddbc17903a381dd325a9
Illya <illamatviuk@gmail.com>
2025-06-25 21:16:18 +0300

    Added Company UnitOfWork.

---
+++ CompanyService.Infrastructure/UnitOfWork/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using CompanyService.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyService.Infrastructure.UnitOfWork.Interfaces {
+    public interface IUnitOfWork {
+        ICompanyRepository Companies {  get; }
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ CompanyService.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using CompanyService.Domain.Interfaces;
+using CompanyService.Infrastructure.DbContext;
+using CompanyService.Infrastructure.UnitOfWork.Interfaces;
+
+namespace CompanyService.Infrastructure.UnitOfWork {
+    public class UnitOfWork(
+        CompanyDbContext context,
+        ICompanyRepository companyRepository
+        ) : IUnitOfWork {
+        public ICompanyRepository Companies => companyRepository;
+        public async Task<int> SaveChangesAsync() =>
+            await context.SaveChangesAsync();
+    }
+}
