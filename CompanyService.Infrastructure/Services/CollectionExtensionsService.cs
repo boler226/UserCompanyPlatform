02cdd692fb40cddb1313cbc209524faa368f1b1f
Illya <illamatviuk@gmail.com>
2025-06-25 21:23:00 +0300

    Added Company CollectionExtionsions service.

---
+++ CompanyService.Infrastructure/Services/CollectionExtensionsService.cs
@@ -0,0 +1,38 @@
+﻿using CompanyService.Domain.Interfaces;
+using CompanyService.Infrastructure.DbContext;
+using CompanyService.Infrastructure.Repositories;
+using CompanyService.Infrastructure.UnitOfWork.Interfaces;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NotificationService.Infrastructure.Consumers;
+
+namespace CompanyService.Infrastructure.Services {
+    public static class CollectionExtensionsService {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString) {
+            Console.WriteLine($"DB CONNECTION: {connectionString}");
+            services.AddDbContext<CompanyDbContext>(options => 
+                options.UseNpgsql(connectionString));
+
+            // Repositories & Services
+            services.AddScoped<ICompanyRepository, CompanyRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork.UnitOfWork>();
+
+            // MassTransit
+            services.AddMassTransit(x => {
+                x.AddConsumer<CompanyCreateConsumer>();
+
+                x.UsingRabbitMq((context, cfg) => {
+                    cfg.Host("rabbitmq", "/", h => {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
+            return services;
+        }
+    }
+}
