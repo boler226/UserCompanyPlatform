f2051ffe9437bde76d19f6b8d1ef0770db74dfbf
Illya <illamatviuk@gmail.com>
2025-06-25 21:15:07 +0300

    Added Entity and DbContext Company.

---
+++ CompanyService.Domain/Entities/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyService.Domain.Entities {
+    public class Company {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ CompanyService.Infrastructure/DbContext/CompanyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CompanyService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CompanyService.Infrastructure.DbContext {
+    public class CompanyDbContext : Microsoft.EntityFrameworkCore.DbContext {
+        public CompanyDbContext(DbContextOptions<CompanyDbContext> options) : base(options) { }
+        public DbSet<Company> Companies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Company>(entity => {
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Name).IsRequired().HasMaxLength(100);
+                entity.Property(c => c.Email).IsRequired().HasMaxLength(100);
+                entity.Property(c => c.Address).IsRequired();
+            });
+        }
+    }
+}
