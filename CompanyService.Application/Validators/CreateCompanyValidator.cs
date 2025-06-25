be8ddc2979bfba31fe218ce7c3fe30408b6814af
Illya <illamatviuk@gmail.com>
2025-06-25 21:18:46 +0300

    Added Create, Update Validators to Company.

---
+++ CompanyService.Application/Validators/CreateCompanyValidator.cs
@@ -0,0 +1,12 @@
+﻿using CompanyService.Application.Commands.CreateCompany;
+using FluentValidation;
+
+namespace CompanyService.Application.Validators {
+    public class CreateCompanyValidator : AbstractValidator<CreateCompanyCommand> {
+        public CreateCompanyValidator() {
+            RuleFor(c => c.Name).NotEmpty().MaximumLength(100);
+            RuleFor(c => c.Email).NotEmpty().EmailAddress();
+            RuleFor(c => c.Address).NotEmpty();
+        }
+    }
+}
---
+++ CompanyService.Application/Validators/UpdateCompanyValidator.cs
@@ -0,0 +1,13 @@
+﻿using CompanyService.Application.Commands.UpdateCompany;
+using FluentValidation;
+
+namespace CompanyService.Application.Validators {
+    public class UpdateCompanyValidator : AbstractValidator<UpdateCompanyCommand> {
+        public UpdateCompanyValidator() {
+            RuleFor(x => x.Id).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(100);
+            RuleFor(c => c.Email).NotEmpty().EmailAddress();
+            RuleFor(c => c.Address).NotEmpty();
+        }
+    }
+}
