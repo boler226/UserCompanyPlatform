fe5d3b78d2819723b2879fdc91b642e2a75516d9
Illya <illamatviuk@gmail.com>
2025-06-26 20:45:05 +0300

    Added UserNotififcation repository.

---
+++ NotificationService.Domain/Interfaces/IUserNotificationRepository.cs
@@ -0,0 +1,9 @@
+﻿using NotificationService.Domain.Entities;
+
+namespace NotificationService.Domain.Interfaces {
+    public interface IUserNotificationRepository {
+        Task AddAsync(UserNotificationSchedule schedule);
+        Task<List<UserNotificationSchedule>> GetUsersToNotifyAsync();
+        Task MarkAsNotifiedAsync(Guid id);
+    }
+}
---
+++ NotificationService.Infrastructure/Reposetories/UserNotificationRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotificationService.Domain.Entities;
+using NotificationService.Domain.Interfaces;
+using NotificationService.Infrastructure.DbContext;
+
+namespace NotificationService.Infrastructure.Reposetories {
+    public class UserNotificationRepository(
+        NotificationDbContext context
+        ) : IUserNotificationRepository {
+        public async Task AddAsync(UserNotificationSchedule schedule) {
+            await context.UserNotificationSchedules.AddAsync(schedule);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<UserNotificationSchedule>> GetUsersToNotifyAsync() {
+            var targetDate = DateTime.UtcNow.Date.AddDays(-2);
+            return await context.UserNotificationSchedules
+                    .Where(n => !n.NotificationSent && n.RegisteredAt.Date <= targetDate)
+                    .ToListAsync();
+        }
+
+        public async Task MarkAsNotifiedAsync(Guid id) {
+            var entity = await context.UserNotificationSchedules.FindAsync(id);
+            if (entity != null) { 
+                entity.NotificationSent = true;
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
