848f88bd518aad2b5f6f9345560024c713f8450e
Illya <illamatviuk@gmail.com>
2025-07-13 20:36:52 +0300

    Added layout with basic style.

--- user-company-front/src/app/app.routes.ts
+++ user-company-front/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/home/home').then(m => m.HomeComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about').then(m => m.AboutComponent),
+  },
+];
