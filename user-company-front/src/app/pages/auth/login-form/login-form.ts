abdc7eb2ebd937e790870731f4b0e24896cca28f
Illya <illamatviuk@gmail.com>
2025-07-14 22:18:02 +0300

    Added login and register forms components.

---
+++ user-company-front/src/app/pages/auth/login-form/login-form.ts
@@ -0,0 +1,32 @@
+import {Component, inject} from '@angular/core';
+import {AuthService} from '../../../core/services/auth.service';
+import {TokenService} from '../../../core/services/token.service';
+import {LoginDto} from '../../../core/models/login.dto';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-login-form',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './login-form.html',
+  standalone: true,
+  styleUrl: '../auth.scss'
+})
+export class LoginFormComponent {
+  email = '';
+  password = '';
+  private authService = inject(AuthService);
+  private tokenService = inject(TokenService);
+
+  onLogin() {
+    const dto: LoginDto = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.authService.login(dto).subscribe((token) => {
+      this.tokenService.setToken(token);
+    });
+  }
+}
---
+++ user-company-front/src/app/pages/auth/register-form/register-form.ts
@@ -0,0 +1,52 @@
+import {Component, inject} from '@angular/core';
+import {RegisterDto} from '../../../core/models/register.dto';
+import {AuthService} from '../../../core/services/auth.service';
+import {TokenService} from '../../../core/services/token.service';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register-form',
+  imports: [
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './register-form.html',
+  standalone: true,
+  styleUrl: '../auth.scss'
+})
+export class RegisterFormComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private tokenService = inject(TokenService);
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    firstname: ['', Validators.required],
+    lastname: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', Validators.required],
+  });
+
+  onRegister() {
+    if (this.form.invalid)
+      return;
+
+    const { email, firstname, lastname, password, confirmPassword } = this.form.value;
+
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    const dto: RegisterDto = {
+      email: email!,
+      firstname: firstname!,
+      lastname: lastname!,
+      password: password!
+    };
+
+    this.authService.register(dto).subscribe((token) => {
+      this.tokenService.setToken(token);
+    });
+  }
+}
